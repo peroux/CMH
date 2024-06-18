535aaf123af4c29a0cb0324182b8ed8ef9474280
AidanMath <aidanmath0331@gmail.com>
2024-06-18 09:37:53 -0500

    Update Guest.java

--- src/main/java/cmh/backend/Guest.java
+++ src/main/java/cmh/backend/Guest.java
@@ -35,7 +35,10 @@ public class Guest {
         this.checkIn = checkIn;
         this.checkOut = checkOut;
         this.otherInfo=otherInfo;
+<<<<<<< Updated upstream
         this.isCheckedIn = false;
+=======
+>>>>>>> Stashed changes
         allGuests.add(this);
     }
 
